809f67c87971c72368aad5da5bd15a043f828f12
Dioneibi-rip <fandubdreams@gmail.com>
2026-05-16 21:07:57 -0400

    Actualizar main-afk.js

--- plugins/main-afk.js
+++ plugins/main-afk.js
@@ -1,22 +1,50 @@
-export function before(m) {
-const user = global.db.data.users[m.sender];
-if (user.afk > -1) {
-conn.reply(m.chat, `${emoji} Dejastes De Estar Inactivo\n${user.afkReason ? 'Motivo De La Inactividad: ' + user.afkReason : ''}\n\n*Tiempo Inactivo: ${(new Date - user.afk).toTimeString()}*`, m)
-user.afk = -1;
-user.afkReason = '';
-}
-const jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])];
-for (const jid of jids) {
-const user = global.db.data.users[jid];
-if (!user) {
-continue;
-}
-const afkTime = user.afk;
-if (!afkTime || afkTime < 0) {
-continue;
-}
-const reason = user.afkReason || '';
-conn.reply(m.chat, `${emoji2} El Usuario Esta Inactivo No Lo Etiquetes.`, m)
-}
-return true;
+export function before(m, { conn }) {
+    const user = global.db.data.users[m.sender];
+
+    const clockString = (ms) => {
+        let h = Math.floor(ms / 3600000);
+        let m = Math.floor(ms / 60000) % 60;
+        let s = Math.floor(ms / 1000) % 60;
+        return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':');
+    };
+
+    if (user && user.afk > -1) {
+        let timeAfk = clockString(new Date() - user.afk);
+        let reasonText = user.afkReason ? `\n         🧇̫͠ ꒰  *𝖬𝗈𝗍𝗂𝗏𝗈:* ${user.afkReason}` : '';
+        
+        let returnText = `> 🍰 𝖣𝖾𝗃𝖺𝗌𝗍𝖾     𝖽𝖾     𝖾𝗌𝗍𝖺𝗋     𝗂𝗇𝖺𝖼𝗍𝗂𝗏𝗈     !
+        
+୨ㅤ࣪ㅤ︶︶︶︶ ㅤ꒰ 🎀 ꒱ㅤ︶︶︶︶ㅤ࣪ㅤ୧
+
+         🍪̮͡ 〣  *𝖳𝗂𝖾𝗆𝗉𝗈     𝖨𝗇𝖺𝖼𝗍𝗂𝗏𝗈:* ${timeAfk}${reasonText}
+         
+> \`𝖡𝗂𝖾𝗇𝗏𝖾𝗇𝗂𝖽𝗈     𝖽𝖾     𝗏𝗎𝖾𝗅𝗍𝖺     ♡\``;
+
+        conn.reply(m.chat, returnText, m);
+        user.afk = -1;
+        user.afkReason = '';
+    }
+
+    const jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])];
+    for (const jid of jids) {
+        const mentionedUser = global.db.data.users[jid];
+        if (!mentionedUser) continue;
+        
+        const afkTime = mentionedUser.afk;
+        if (!afkTime || afkTime < 0) continue;
+        
+        const reason = mentionedUser.afkReason || '𝖲𝗂𝗇     𝖾𝗌𝗉𝖾𝖼𝗂𝖿𝗂𝖼𝖺𝗋';
+        let timeAfk = clockString(new Date() - afkTime);
+
+        let mentionText = `> 💤 𝖤𝗅     𝗎𝗌𝗎𝖺𝗋𝗂𝗈     𝖾𝗌𝗍𝖺́     𝗂𝗇𝖺𝖼𝗍𝗂𝗏𝗈     ,     𝗇𝗈     𝗅𝗈     𝖾𝗍𝗂𝗊𝗎𝖾𝗍𝖾𝗌     . . .
+        
+୨ㅤ࣪ㅤ︶︶︶︶ ㅤ꒰ 🌸 ꒱ㅤ︶︶︶︶ㅤ࣪ㅤ୧
+
+         🏩ິࣼᝒ  *𝖴𝗌𝗎𝖺𝗋𝗂𝗈:* @${jid.split('@')[0]}
+         🍥̶̸̑ 〣  *𝖬𝗈𝗍𝗂𝗏𝗈:* ${reason}
+         🥛̫̌ യ  *𝖳𝗂𝖾𝗆𝗉𝗈:* ${timeAfk}`;
+
+        conn.reply(m.chat, mentionText, m, { mentions: [jid] });
+    }
+    return true;
 }
