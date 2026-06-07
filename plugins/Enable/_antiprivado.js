186db7a4d88b48020fb064defccb88736dfbba9c
Dioneibi-rip <fandubdreams@gmail.com>
2026-06-07 17:24:15 -0400

    Actualizar _antiprivado.js

--- plugins/Enable/_antiprivado.js
+++ plugins/Enable/_antiprivado.js
@@ -7,16 +7,17 @@ if (m.isBaileys && m.fromMe) return !0
 if (m.fromMe || isOwner || isROwner) return !1
 if (m.isGroup) return !1
 if (!m.message) return !0
-if (m.chat === '120363322713003916@newsletter') return !0
+if (m.chat === '120363322713003916@newsletter') return !1
 if (isEssentialPrivateCommand(m.text || '')) return !1
-const chat = global.db.data.chats[m.chat] || (global.db.data.chats[m.chat] = {})
-if (chat.isBanned === false) return !1
-if (chat.isBanned === true) {
+const chat = global.db?.data?.chats?.[m.chat]
+if (chat?.isBanned === false) return !1
+if (chat?.isBanned === true) {
 m.__pluginHalt = true
 return !0
 }
-const bot = global.db.data.settings[this.user.jid] || (global.db.data.settings[this.user.jid] = {})
-const mode = bot.antiPrivate
+const settings = global.db?.data?.settings?.[this.user.jid] || {}
+const mode = settings.antiPrivate
+if (!mode || mode === 0 || mode === false) return !1
 if (mode === 1 || mode === true) {
 await m.reply(`${global.emoji || '✦'} Hola @${m.sender.split`@`[0]}, mi creador ha desactivado los comandos en chats privados. Serás bloqueado; si quieres usar los comandos del bot te invito a unirte al grupo principal.\n\n${global.gp1 || ''}`, false, {mentions: [m.sender]})
 await this.updateBlockStatus(m.chat, 'block')
