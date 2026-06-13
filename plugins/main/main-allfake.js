688b5f82c210ed7f8a65c46c167833fb5d839e68
Dioneibi-rip <fandubdreams@gmail.com>
2026-06-13 10:52:30 -0400

    Actualizar main-allfake.js

--- plugins/main/main-allfake.js
+++ plugins/main/main-allfake.js
@@ -4,136 +4,173 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 import moment from 'moment-timezone'
 const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = pkg
+
 var handler = m => m
-handler.all = async function (m, { conn }) {
-if (!conn) return
+
+handler.all = async function (m) {
+
 global.getBuffer = async function getBuffer(url, options) {
-try {
-options ? options : {}
-var res = await axios({
-method: "get",
-url,
-headers: {
-'DNT': 1,
-'User-Agent': 'GoogleBot',
-'Upgrade-Insecure-Request': 1
-},
-...options,
-responseType: 'arraybuffer'
-})
-return res.data
-} catch (e) {
-console.log(`Error : ${e}`)
+  try {
+    options ? options : {}
+    var res = await axios({
+      method: "get",
+      url,
+      headers: {
+        'DNT': 1,
+        'User-Agent': 'GoogleBot',
+        'Upgrade-Insecure-Request': 1
+      },
+      ...options,
+      responseType: 'arraybuffer'
+    })
+    return res.data
+  } catch (e) {
+    console.log(`Error : ${e}`)
+  }
 }
-}
+
+// Lista de iconos
 const iconUrls = [
-"https://files.catbox.moe/ahp3bc.jpeg", "https://files.catbox.moe/ffkx61.jpg",
-"https://files.catbox.moe/uc272d.webp", "https://files.catbox.moe/nuoard.jpg",
-"https://files.catbox.moe/edsflw.jpg", "https://files.catbox.moe/nuoard.jpg",
-"https://files.catbox.moe/ilkgfh.webp", "https://files.catbox.moe/fslr4h.jpg",
-"https://files.catbox.moe/k25pcl.jpg", "https://files.catbox.moe/5qglcn.jpg",
-"https://files.catbox.moe/nvhomc.jpeg", "https://files.catbox.moe/d81jgr.jpg",
-"https://files.catbox.moe/k25pcl.jpg", "https://files.catbox.moe/6x9q51.jpg",
-"https://files.catbox.moe/i7vsnr.jpg", "https://files.catbox.moe/e9zgbu.jpg",
-"https://files.catbox.moe/nuoard.jpg", "https://files.catbox.moe/jm6j5b.jpeg",
-"https://files.catbox.moe/jobvjq.jpg", "https://files.catbox.moe/iph9xr.jpeg",
-"https://files.catbox.moe/z962x9.jpg", "https://files.catbox.moe/k8griq.jpeg",
-"https://files.catbox.moe/fslr4h.jpg", "https://files.catbox.moe/104xtw.jpeg",
-"https://files.catbox.moe/ffkx61.jpg", "https://files.catbox.moe/pjuo2b.jpg",
-"https://files.catbox.moe/jobvjq.jpg", "https://files.catbox.moe/7bn1pf.jpg",
-"https://files.catbox.moe/z962x9.jpg", "https://files.catbox.moe/fe6pw6.jpeg",
-"https://files.catbox.moe/fslr4h.jpg"
+  "https://files.catbox.moe/ahp3bc.jpeg", "https://files.catbox.moe/ffkx61.jpg",
+  "https://files.catbox.moe/uc272d.webp", "https://files.catbox.moe/nuoard.jpg",
+  "https://files.catbox.moe/edsflw.jpg", "https://files.catbox.moe/nuoard.jpg",
+  "https://files.catbox.moe/ilkgfh.webp", "https://files.catbox.moe/fslr4h.jpg",
+  "https://files.catbox.moe/k25pcl.jpg",  "https://files.catbox.moe/5qglcn.jpg",
+  "https://files.catbox.moe/nvhomc.jpeg",  "https://files.catbox.moe/d81jgr.jpg",
+  "https://files.catbox.moe/k25pcl.jpg",  "https://files.catbox.moe/6x9q51.jpg",
+  "https://files.catbox.moe/i7vsnr.jpg",  "https://files.catbox.moe/e9zgbu.jpg",
+  "https://files.catbox.moe/nuoard.jpg", "https://files.catbox.moe/jm6j5b.jpeg",
+  "https://files.catbox.moe/jobvjq.jpg", "https://files.catbox.moe/iph9xr.jpeg",
+  "https://files.catbox.moe/z962x9.jpg", "https://files.catbox.moe/k8griq.jpeg",
+  "https://files.catbox.moe/fslr4h.jpg", "https://files.catbox.moe/104xtw.jpeg",
+  "https://files.catbox.moe/ffkx61.jpg", "https://files.catbox.moe/pjuo2b.jpg",
+  "https://files.catbox.moe/jobvjq.jpg",  "https://files.catbox.moe/7bn1pf.jpg",
+  "https://files.catbox.moe/z962x9.jpg", "https://files.catbox.moe/fe6pw6.jpeg",
+  "https://files.catbox.moe/fslr4h.jpg"
 ]
+
+// Función para elegir y descargar un icono aleatorio
 function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]
+  return list[Math.floor(Math.random() * list.length)]
 }
+
 const iconUrl = pickRandom(iconUrls)
 global.icono = await getBuffer(iconUrl)
+
 global.fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+
+//creador y otros
 global.creador = 'Wa.me/18294868853'
-global.ofcbot = `${(conn.user && conn.user.jid) ? conn.user.jid.split('@')[0] : 'bot'}`
+global.ofcbot = `${conn.user.jid.split('@')[0]}`
 global.asistencia = 'Wa.me/18294868853'
 global.namechannel = '⏤͟͞ू⃪፝͜⁞⟡『 𝐓͢ᴇ𝙖፝ᴍ⃨ 𝘾𝒉꯭𝐚𝑛𝑛𝒆𝑙: 𝑹ᴜ⃜ɓ𝑦-𝑯ᴏ𝒔𝑯𝙞꯭𝑛𝒐 』࿐⟡'
 global.namechannel2 = '⟡『 𝐓𝐞𝐚𝐦 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: 𝑹𝒖𝒃𝒚-𝑯𝒐𝒔𝒉𝒊𝒏𝒐 』⟡'
-global.namegrupo = '⏤͟͞ू⃪ 𝑹𝒖𝒃𝒚-𝑯𝒐𝒔𝒉𝒊𝒏𝒐-𝐵ot ⌬⃝𓆩⚘𓆪 𝐎𝐟𝐟𝐢cial'
+global.namegrupo = '⏤͟͞ू⃪ 𝑹𝒖𝒃𝒚-𝑯𝒐𝒔𝒉𝒊𝒏𝒐-𝐵ot ⌬⃝𓆩⚘𓆪 𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥'
 global.namecomu = '⏤͟͞ू⃪ 𝑹𝒖𝒃𝒚-𝑯𝒐𝒔𝒉𝒊𝒏𝒐 ✦⃝𖤐 𝑪𝒐𝒎𝒎𝒖𝒏𝒊𝒕𝒚'
 global.listo = '❀ *Aquí tienes ฅ^•ﻌ•^ฅ*'
 global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
+
+//Ids channel
 global.canalIdM = ["120363335626706839@newsletter", "120363335626706839@newsletter"]
 global.canalNombreM = ["⏤͟͞ू⃪፝͜⁞⟡『 𝐓͢ᴇ𝙖፝ᴍ⃨ 𝘾𝒉꯭𝐚𝑛𝑛𝒆𝑙: 𝑹ᴜ⃜ɓ𝑦-𝑯ᴏ𝒔𝑯𝙞꯭𝑛𝒐 』࿐⟡", "⟡『 𝐓𝐞𝐚𝐦 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: 𝑹𝒖𝒃𝒚-𝑯𝒐𝒔𝒉𝒊𝒏𝒐 』⟡"]
-let randomC = Math.floor(Math.random() * global.canalIdM.length)
-global.channelRD = { id: global.canalIdM[randomC], name: global.canalNombreM[randomC] }
-const d = new Date(new Date().getTime() + 3600000)
-global.d = d
+global.channelRD = await getRandomChannel()
+
+//fechas
+global.d = new Date(new Date + 3600000)
 global.locale = 'es'
-global.dia = d.toLocaleDateString(global.locale, {weekday: 'long'})
+global.dia = d.toLocaleDateString(locale, {weekday: 'long'})
 global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'})
 global.mes = d.toLocaleDateString('es', {month: 'long'})
 global.año = d.toLocaleDateString('es', {year: 'numeric'})
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
+
+//Reacciones De Comandos
 global.rwait = '🕒'
 global.done = '✅'
 global.error = '✖️'
 global.msm = '⚠︎'
+
+//Emojis Rubyi Bot
 global.emoji = '🍨'
 global.emoji2 = '🍭'
 global.emoji3 = '🌺'
 global.emoji4 = '💗'
 global.emoji5 = '🍡'
-let ems = [global.emoji, global.emoji2, global.emoji3, global.emoji4]
-global.emojis = ems[Math.floor(Math.random() * ems.length)]
+global.emojis = [emoji, emoji2, emoji3, emoji4].getRandom()
+
+//Espera
 global.wait = '⚘𖠵⃕❖𖥔 𝑪𝒂𝒓𝒈𝒂𝒏𝒅𝒐...ꪶꪾ❍̵̤̂ꫂ\n❝ 𝐴𝑔𝑢𝑎𝑟𝑑𝑒 𝑢𝑛 𝑚𝑜𝑚𝑒𝑛𝑡𝑜 ❞';
+
+//Enlaces
 var canal = 'https://whatsapp.com/channel/0029VakLbM76mYPPFL0IFI3P'
 let canal2 = 'https://whatsapp.com/channel/0029VakLbM76mYPPFL0IFI3P'
 var git = 'https://github.com/Dioneibi-rip'
 var github = 'https://github.com/Dioneibi-rip/Ruby-Hoshino-Bot' 
 let correo = 'ulcidecono@gmail.com'
-let rds = [canal, canal2, git, github, correo]
-global.redes = rds[Math.floor(Math.random() * rds.length)]
-try {
+global.redes = [canal, canal2, git, github, correo].getRandom()
+
+//Imagen aleatoria
 let category = "imagen"
 const db = './src/database/db.json'
 const db_ = JSON.parse(fs.readFileSync(db))
 const random = Math.floor(Math.random() * db_.links[category].length)
 const randomlink = db_.links[category][random]
 const response = await fetch(randomlink)
-global.icons = await response.buffer()
-} catch (e) { console.error(e) }
+const rimg = await response.buffer()
+global.icons = rimg
+
+// Saludo por hora
 var ase = new Date(); var hour = ase.getHours();
 switch(hour){
-case 0: case 1: case 2: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break;
-case 3: case 4: case 5: case 6: case 8: case 9: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break;
-case 7: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌅'; break;
-case 10: case 11: case 12: case 13: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break;
-case 14: case 15: case 16: case 17: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break;
-default: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'
+  case 0: case 1: case 2: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break;
+  case 3: case 4: case 5: case 6: case 8: case 9: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break;
+  case 7: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌅'; break;
+  case 10: case 11: case 12: case 13: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break;
+  case 14: case 15: case 16: case 17: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break;
+  default: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'
 }
 global.saludo = hour
+
+//tags
 global.nombre = m.pushName || 'Anónimo'
 global.taguser = '@' + m.sender.split("@")[0]
 var more = String.fromCharCode(8206)
 global.readMore = more.repeat(850)
+
 global.packsticker = `${global.nombre}`
 global.packsticker2 = `𝚁𝚄𝙱𝚈 𝙱𝙾𝚃 𝙼𝙳 ˃ 𖥦 ˂`
+
+// rcanaɭ con icono como buffer
 global.rcanal = {
-contextInfo: {
-mentionedJid: [], 
-isForwarded: true,
-forwardingScore: 999,
-forwardedNewsletterMessageInfo: {
-newsletterJid: '120363335626706839@newsletter',
-newsletterName: '⏤͟͞ू⃪፝͜⁞⟡『 𝐓͢ᴇ𝙖፝ᴍ⃨ 𝘾𝒉꯭𝐚𝑛𝑛𝒆𝑙: 𝑹ᴜ⃜ɓ𝑦-𝑯ᴏ𝒔𝑯𝙞꯭𝑛𝒐 』࿐⟡',
-serverMessageId: -1
-},
-externalAdReply: {
-title: global.packname || 'Ruby Bot',
-body: global.dev || 'Dioneibi',
-thumbnail: global.icons,
-sourceUrl: global.redes,
-mediaType: 1,
-renderLargerThumbnail: false
+  contextInfo: {
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: channelRD.id,
+      serverMessageId: 100,
+      newsletterName: channelRD.name,
+    },
+    externalAdReply: {
+      showAdAttribution: true,
+      title: botname,
+      body: dev,
+      mediaUrl: null,
+      description: null,
+      previewType: "PHOTO",
+      thumbnail: global.icono,
+      sourceUrl: global.redes,
+      mediaType: 1,
+      renderLargerThumbnail: false
+    },
+  }
 }
+
 }
-}
-}
+
 export default handler
+
+async function getRandomChannel() {
+  let randomIndex = Math.floor(Math.random() * canalIdM.length)
+  let id = canalIdM[randomIndex]
+  let name = canalNombreM[randomIndex]
+  return { id, name }
+}
