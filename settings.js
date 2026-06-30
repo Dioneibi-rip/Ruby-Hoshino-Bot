9c2c808fb9f8d352bb84bc215e8a5a01668c3cf5
Dioneibi-rip <fandubdreams@gmail.com>
2026-06-30 13:18:53 -0400

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -68,8 +68,6 @@ global.etiqueta = 'ˑ 𓈒 𐔌 D͙i͙o͙n͙e͙i͙b͙i͙-r͙i͙p͙ ͡꒱ ۫'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.banner = 'https://files.catbox.moe/b93cts.jpg'
-global.avatar = 'https://qu.ax/RYjEw.jpeg'
 global.catalogo = fs.readFileSync('./src/catalogo.jpg')
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
