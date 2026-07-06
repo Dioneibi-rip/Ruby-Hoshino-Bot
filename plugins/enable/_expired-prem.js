ee807c60c128a93650ea378e1f4a42ca52c978ac
Dioneibi-rip <fandubdreams@gmail.com>
2026-07-06 14:04:58 -0400

    Actualizar _expired-prem.js

--- plugins/enable/_expired-prem.js
+++ plugins/enable/_expired-prem.js
@@ -1,6 +1,6 @@
-const handler = (m) => m;
+con handler = (m) => m;
 
-handler.all = async function all(m) {
+handler.al = async function all(m) {
 for (const user of Object.values(global.db.listUsers())) {
 if (user.premiumTime != 0 && user.premium) {
 if (new Date() * 1 >= user.premiumTime) {
