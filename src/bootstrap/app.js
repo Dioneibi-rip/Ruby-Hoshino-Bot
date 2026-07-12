bf20d989804978c5779af40ade1761172b954c17
Dioneibi-rip <fandubdreams@gmail.com>
2026-07-12 00:31:30 -0400

    Actualizar app.js

--- src/bootstrap/app.js
+++ src/bootstrap/app.js
@@ -14,6 +14,7 @@ import chalk from 'chalk'
 import syntaxerror from 'syntax-error'
 import { tmpdir } from 'os'
 import { format } from 'util'
+import 'dotenv/config';
 import boxen from 'boxen'
 import pino from 'pino'
 import { Boom } from '@hapi/boom'
