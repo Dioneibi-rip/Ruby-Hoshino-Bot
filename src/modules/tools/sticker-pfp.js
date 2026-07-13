5828820f38918f355338b06d7963ab2da189a95d
Dioneibi-rip <fandubdreams@gmail.com>
2026-07-13 01:37:13 -0400

    Actualizar sticker-pfp.js

--- src/modules/tools/sticker-pfp.js
+++ src/modules/tools/sticker-pfp.js
@@ -1,6 +1,9 @@
+import { normalizeIdentityJid } from '../../core/identity-utils.js'
+
 let handler = async (m, { conn }) => {
-const who = m.mentionedJid?.[0] || m.quoted?.sender || (m.fromMe ? conn.user.jid : m.sender)
-const name = await Promise.resolve(conn.getName(who))
+let target = m.mentionedJid?.[0] || m.quoted?.sender || (m.fromMe ? conn.user.jid : m.sender)
+const who = await normalizeIdentityJid(conn, target)
+const name = await conn.getName(who)
 const pp = await conn.profilePictureUrl(who, 'image').catch(() => 'https://files.catbox.moe/xr2m6u.jpg')
 await conn.sendFile(m.chat, pp, 'profile.jpg', `*Foto de perfil de ${name || `@${who.split('@')[0]}`}*`, m, false, { mentions: [who] })
 }
